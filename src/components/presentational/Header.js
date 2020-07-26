29a712540f3fcc65eb36e9bec6a70178039060af
r.i <ri@TheLaughingMan.local>
2020-07-27 00:22:25 +0900

    update:create components directory and header component

---
+++ src/components/presentational/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'assets/style/Header.scss'
+
+const Header = () => {
+  return (
+    <div
+      className="header"
+    >
+      Header Area
+    </div>
+  );
+}
+
+export default Header
