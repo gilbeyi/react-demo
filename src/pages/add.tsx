2f915bcadbcf156c8afac1fce8b9e62323f9bcf6
r.i <pechuelii33@gmail.com>
2021-05-03 21:21:41 +0900

    update:Inputコンポーネント配置

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import AppInput from 'components/atoms/AppInput'
 
 const AddPage = () => {
+  const [value, setValue] = useState('')
+  useEffect(() => {
+    setValue('初期値')
+  }, [])
   return (
     <div>
       Add
+      <div>
+        <AppInput
+          value={value}
+          onChange={setValue}
+        />
+      </div>
     </div>
   )
 }
